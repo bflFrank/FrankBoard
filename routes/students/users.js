a24b703353bff938fb6bc1a466c8c29dd82d043a
Manuel Soto <sotomanuel41@icloud.com>
2018-04-11 17:01:03 -0500

    changed user view

--- routes/students/users.js
+++ routes/students/users.js
@@ -4,20 +4,11 @@ var db = require('../../db/db')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.all('SELECT * FROM users WHERE studentID = 12345', function(err, rows){
-    if(err) throw err;
-    var student = rows[0];
-    if(!student){
-      console.log('No student found');
-      return;
-    }
 
     res.render('./studentViews/studentView', {
       title: 'Student',
       message: 'Student',
-      student: student
     });
-  });
 
 });
 
