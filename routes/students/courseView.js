575ab647433224dcc325524c333eb4703b68044a
Frank Orechio <forechio@gmail.com>
2018-04-25 15:39:58 -0500

    GPA Changes

--- routes/students/courseView.js
+++ routes/students/courseView.js
@@ -9,16 +9,23 @@ router.get('/', function (req, res, next) {
     db.all('SELECT subject, grade, grades.name as description FROM courses left join grades on courses.idCourse = grades.idCourse where grades.idUser = ?', req.session.idUser, function (err, rows) {
         if (err) throw err;
         res.locals.rows = rows;
-        db.get("SELECT sum(grade) as total, count(grade) as amount FROM courses left join grades on courses.idCourse = grades.idCourse where grades.idUser = ?", req.session.idUser, function(err, row){
-            if(row.total / row.amount >= 90)
-                res.locals.gpa = 4;
-            else if(row.total / row.amount >= 80)
-                res.locals.gpa = 3;
-            else if(row.total / row.amount >= 70)
-                res.locals.gpa = 2;
-            else if(row.total / row.amount >= 60)
-                res.locals.gpa = 1;
-            else res.locals.gpa = 0;
+        db.all("SELECT courses.idCourse, sum(grade) as total, count(grade) as amount FROM courses left join grades on courses.idCourse = grades.idCourse where grades.idUser = ? group by courses.idCourse", req.session.idUser, function(err, rows){
+            var gpa = 0;
+            var tot = 0;
+            console.log(rows);
+            for(var i = 0; i < rows.length; i++){
+                if(rows[i].total / rows[i].amount >= 90)
+                    gpa += 4;
+                else if(rows[i].total / rows[i].amount >= 80)
+                    gpa += 3;
+                else if(rows[i].total / rows[i].amount >= 70)
+                    gpa += 2;
+                else if(rows[i].total / rows[i].amount >= 60)
+                    gpa += 1;
+                else res.locals.gpa = 0;
+                tot++;
+            }
+            res.locals.gpa = gpa / tot;
             res.render('./studentViews/courseView');
         });
     });
